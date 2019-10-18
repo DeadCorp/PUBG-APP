c80aa7dde40d406146d31b03905e1ca3680e8dce
DeadCorp <cipidris@gmail.com>
2019-10-19 02:24:32 +0300

    small search fix

--- PUBG APP/PUBG APP/MainPage.xaml.cs
+++ PUBG APP/PUBG APP/MainPage.xaml.cs
@@ -51,46 +51,51 @@ namespace PUBG_APP
             }
         }
         private void setc(string q) => Label_Player_Id.Text = q;
+
+        private void Player_Id_Completed(object sender, FocusEventArgs e)
+        {
+            Player_Id_Case = Player_Id.Text;
+        }
         /*  MainAsync().Wait();
-          async Task MainAsync()
-          {
-              string maxkill = null;
-              /*string id_name = null;
-              string id_match = null;
-              PubgNetClient client = new PubgNetClient("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0ZjYyODUxMC1kM2UwLTAxMzctZDdhNC0wZjNhMTg5NGE0ZTciLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTcxNDA4NDA3LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImNpcGlkcmlzLWdtYWlsIn0.vTbdRV5CgGMOUUU7zDmMojlSULLafjhfFbBfEkbidjI");
-              var players = await client.GetPlayersByUsernames(new string[]{"DeadCorporation"});
+ async Task MainAsync()
+ {
+     string maxkill = null;
+     /*string id_name = null;
+     string id_match = null;
+     PubgNetClient client = new PubgNetClient("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0ZjYyODUxMC1kM2UwLTAxMzctZDdhNC0wZjNhMTg5NGE0ZTciLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTcxNDA4NDA3LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImNpcGlkcmlzLWdtYWlsIn0.vTbdRV5CgGMOUUU7zDmMojlSULLafjhfFbBfEkbidjI");
+     var players = await client.GetPlayersByUsernames(new string[]{"DeadCorporation"});
 
 
-              var season2 = await client.GetSeasons();
-               foreach(var s in players.Data)
-                {
-                    id_name = s.Id;
-                }
-              foreach (var i in season2.Data)
-              {
+     var season2 = await client.GetSeasons();
+      foreach(var s in players.Data)
+       {
+           id_name = s.Id;
+       }
+     foreach (var i in season2.Data)
+     {
 
-                  if (i.Attributes.IsCurrentSeason)
-                  {
-                       id_match = i.Id;
-                  }
-                  var season = await client.GetSeasonStatsForPlayer(id_name,id_match);
-              }
+         if (i.Attributes.IsCurrentSeason)
+         {
+              id_match = i.Id;
+         }
+         var season = await client.GetSeasonStatsForPlayer(id_name,id_match);
+     }
 
-              *//*
-              string id_name = null;
-              PubgNetClient client = new PubgNetClient("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0ZjYyODUxMC1kM2UwLTAxMzctZDdhNC0wZjNhMTg5NGE0ZTciLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTcxNDA4NDA3LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImNpcGlkcmlzLWdtYWlsIn0.vTbdRV5CgGMOUUU7zDmMojlSULLafjhfFbBfEkbidjI");
-              var players = await client.GetPlayersByUsernames(new string[] { "DeadCorporation" });
-              foreach (var s in players.Data)
-              {
-                  id_name = s.Id;
-                  maxkill = id_name;
-                  break;
-              }
-              Console.WriteLine(maxkill);
-              System.Console.ReadKey();
+     *//*
+     string id_name = null;
+     PubgNetClient client = new PubgNetClient("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0ZjYyODUxMC1kM2UwLTAxMzctZDdhNC0wZjNhMTg5NGE0ZTciLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTcxNDA4NDA3LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImNpcGlkcmlzLWdtYWlsIn0.vTbdRV5CgGMOUUU7zDmMojlSULLafjhfFbBfEkbidjI");
+     var players = await client.GetPlayersByUsernames(new string[] { "DeadCorporation" });
+     foreach (var s in players.Data)
+     {
+         id_name = s.Id;
+         maxkill = id_name;
+         break;
+     }
+     Console.WriteLine(maxkill);
+     System.Console.ReadKey();
 
-          }
-    */
+ }
+*/
 
 
     }
